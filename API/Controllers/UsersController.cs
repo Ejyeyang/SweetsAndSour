b19e853482aa397d439c038999a44184bb1985f0
Ejyeyang <75270307+Ejyeyang@users.noreply.github.com>
2021-04-29 11:24:49 -0700

    created memberdto to get data in certain format from resposne of api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -26,18 +26,16 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _userRepository.GetUsersAsync();
+            var users = await _userRepository.GetMembersAsync();
+            return Ok(users);
 
-            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-
-            return Ok(usersToReturn);
         }
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            var user = await _userRepository.GetUserByUsernameAsync(username);
-            return _mapper.Map<MemberDto>(user);
+            var user = await _userRepository.GetMemberAsync(username);
+            return user;
         }
     }
 }
