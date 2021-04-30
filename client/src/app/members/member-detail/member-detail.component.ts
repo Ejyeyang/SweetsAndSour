b74343ca188ca280c923df8134e4794bb678cb1d
Ejyeyang <75270307+Ejyeyang@users.noreply.github.com>
2021-04-30 09:29:44 -0700

    using activatedroute when a user clicks a profile button it will get the member by username and load the indiviual user on the member-detail component through the angular routes

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -7,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member;
 
-  constructor() { }
+  constructor(
+    private memberService: MembersService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member => {
+      this.member = member;
+    });
   }
 
 }
